a89aee0c70c5582870e934c3abb81e9fff2a9037
louisabraham <louis.abraham@yahoo.fr>
2018-08-10 11:08:12 +0200

    fix installation bug

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ def read(fname):
 
 setup(
     name='ffpass',
-    version='0.2.1',
+    version='0.3.0',
     author='Louis Abraham',
     license='MIT',
     author_email='louis.abraham@yahoo.fr',
     description='Import and Export passwords for Firefox',
     long_description=read('README.rst'),
     url='https://github.com/louisabraham/ffpass',
-    use_scm_version=False,
+    packages=['ffpass'],
     install_requires=['pyasn1', 'Crypto'],
     python_requires='>=3',
     entry_points={'console_scripts': ['ffpass = ffpass:main']},
