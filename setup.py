e36b7e635ecacc816eee1a52bf92fe39ffb31628
louisabraham <louis.abraham@yahoo.fr>
2018-12-07 16:22:57 +0100

    v0.4.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name="ffpass",
-    version="0.4.1",
+    version="0.4.2",
     author="Louis Abraham",
     license="MIT",
     author_email="louis.abraham@yahoo.fr",
